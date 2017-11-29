b9e791dc3f6b8d407ff36352bc6df99e0f92a773
Adam Lyon <lyon@fnal.gov>
2017-11-29 09:03:29 -0600

    Add better comments

--- src/events.jl
+++ src/events.jl
@@ -22,16 +22,29 @@ end
 
 # See https://docs.julialang.org/en/stable/stdlib/collections/#lib-collections-iteration-1 and
 # see https://docs.julialang.org/en/stable/manual/interfaces/#man-interface-iteration-1
-# for how this works as a for loop
-
+# for how this works as a for loop. That is,
+#   for i = I   # or  "for i in I"
+#     ...
+#   end
+#
+#   is translated into
+#
+#  state = start(I)
+#  while !done(I, state)
+#      (i, state) = next(I, state)
+#      ...
+#  end
+#
+#  So need to define start, next and done. eltype improves efficiency
+#
 Base.start(::HeistEvents) = nothing  # Don't do anything at the start --
                                      # next will make the HeistEvent -- this allows us to see the first event
 
-Base.next(hes::HeistEvents, state::Void) = (he = HeistEvent(hes.hf) ; (he, he) )  # Multiple dispatch in action!
+Base.next(hes::HeistEvents, state::Void)       = (he = HeistEvent(hes.hf) ; (he, he) )  # Multiple dispatch in action!
 Base.next(hes::HeistEvents, state::HeistEvent) = (nextEvent(state), state)
 
-Base.done(hes::HeistEvents, state::Void) = false
+Base.done(hes::HeistEvents, state::Void)       = false
 Base.done(hes::HeistEvents, state::HeistEvent) = atEnd(state) ||
-                                         (hes.nDesired > 0 && state.nSeen ≥ hes.nDesired)
+                                                 (hes.nDesired > 0 && state.nSeen ≥ hes.nDesired)
 
 Base.eltype(::Type{HeistEvents}) = HeistEvent
--- src/getRecord.jl
+++ src/getRecord.jl
@@ -5,6 +5,7 @@
 function getRecord(he::HeistEvent, rs::HeistRecordSpec)
 
    # The below builds cxxt"SomeType"
+   # See https://discourse.julialang.org/t/how-to-build-a-string-for-icxx/7354
    s = @eval @cxxt_str $(rs.productName)
 
    return icxx"return $(he.galleryEvent).getValidHandle<$s>($(rs.artInputTag));"
